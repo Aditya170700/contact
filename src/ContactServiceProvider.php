0851c1ed65dd5cfad8e5fa15ccc3be1e2501c89b
adityaricki <52729836+Aditya170700@users.noreply.github.com>
2022-04-24 17:33:04 +0700

    Publish views

--- src/ContactServiceProvider.php
+++ src/ContactServiceProvider.php
@@ -15,7 +15,8 @@ class ContactServiceProvider extends ServiceProvider
         $this->publishes([
             __DIR__ . '/config/contact.php' => config_path('contact.php'),
             __DIR__ . '/database/migrations/' => database_path('migrations'),
-        ], 'config');
+            __DIR__ . '/views' => resource_path('views/vendor/contact'),
+        ]);
     }
 
     public function register()
